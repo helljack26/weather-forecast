419a34a494f2a1b364b04f26032ca46acfa757d2
helljack26 <helljack22@gmail.com>
2021-10-25 20:58:09 +0300

    Check4

--- script/script.js
+++ script/script.js
@@ -63,8 +63,8 @@ function renderLastOnLoad() {
     if (localStorage[ 0 ] == null) {
         return
     }
-    console.log(localStorage[ 0 ] );
-    if ( localStorage[ 0 ] != undefined ) {
+    // console.log(localStorage[ 0 ] );
+    if ( localStorage[ 0 ] != undefined && localStorage[ 0 ] != null ) {
         console.log('aga');
         lastQueryBlock.innerHTML = '';
         lastCityContainer.style.display = 'block'
